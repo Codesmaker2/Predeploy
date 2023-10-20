eb125827f039122dcb84f18c68730a83d7a4449f
Codesmaker2 <144585960+Codesmaker2@users.noreply.github.com>
2023-10-21 01:46:16 +0800

    Update OrderSuccessPage

--- frontend/src/pages/OrderSuccessPage.jsx
+++ frontend/src/pages/OrderSuccessPage.jsx
@@ -4,13 +4,15 @@ import Header from "../components/Layout/Header";
 import Lottie from "react-lottie";
 import animationData from "../Assests/animations/animation_llnqvst1.json";
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 
 const OrderSuccessPage = () => {
-  
   return (
     <div>
-      <Header />
+      <div className="400px:hidden max-400px:hidden">
+      <Header /> 
+      </div>
       <Success />
       {/* <Footer /> */}
     </div>
@@ -18,7 +20,7 @@ const OrderSuccessPage = () => {
 };
 
 const Success = () => {
-  const {user} = useSelector((state) => state.user);
+  const { user } = useSelector((state) => state.user);
   const defaultOptions = {
     loop: false,
     autoplay: true,
@@ -28,14 +30,30 @@ const Success = () => {
     },
   };
   return (
-    <div>
-      <Lottie options={defaultOptions} width={300} height={300} />
-      <h5 className="text-center mb-14 text-[25px] text-[#000000a1]">
-      Hi, {user?.name}<br />
+    <div className="mt-10">
+      <Lottie options={defaultOptions} width={220} height={90} />
+      <h5 className="text-center mb-5 text-[22px] text-[#000000a1]">
+        Hi, {user?.name}
+        <br />
         Your order is successful ✔️
       </h5>
-      <br />
-      <br />
+     <Link to={"/"}>
+     <div className="flex items-center justify-center">
+        <button
+          type="Continues Order"
+          className="bg-blue-500 h-10 w-[50%] text-white border hover:bg-white hover:text-blue-500 hover:border-blue-500  mb-4 rounded-lg"
+        >
+          Continues Order
+        </button>
+      </div>
+     
+     </Link>
+      <div className="relative mx-4 800px:hidden  h-80 overflow-hidden rounded-xl bg-blue-gray-500 bg-clip-border text-white shadow-lg shadow-blue-gray-500/40 bg-gradient-to-r from-blue-500 to-blue-600">
+        <img
+          src="https://img.freepik.com/free-vector/order-confirmed-concept-illustration_114360-1486.jpg?size=626&ext=jpg&ga=GA1.1.1737282160.1690642300&semt=ais"
+          alt=""
+        />
+      </div>
     </div>
   );
 };
