c47e2234b6ae534c430aec23ecfefa249afbed2e
Codesmaker2 <144585960+Codesmaker2@users.noreply.github.com>
2023-11-09 22:27:41 +0800

    Update NavButton.jsx

--- frontend/src/components/Route/Hero/NavButton.jsx
+++ frontend/src/components/Route/Hero/NavButton.jsx
@@ -11,14 +11,14 @@ const NavButton = () => {
     const [openCart, setOpenCart] = useState(false);
     const { cart } = useSelector((state) => state.cart);
   return (
-    <div className="fixed bottom-0 left-0 w-full bg-white border-t border-gray-300 800px:hidden ">
+    <div className="fixed h-[10vh] bottom-0 left-0 w-full bg-white border-t border-gray-300 rounded-t-[20px] 800px:hidden ">
     <div className="container mx-auto  ">
       <div className="flex  w-full">
         <div className="flex w-full ">
           <div className="flex grid-flow-col-5 justify-between w-full">
            <Link to={"/profile"}><BiUser size={25} className='m-4 rounded-t-lg rounded-b-none'/></Link>
            <Link to={"/inbox"}><BiMessageDots size={25} className='m-4'/></Link>
-           <Link to={"/"}><MdHome size={35}  className='m-2 border rounded-full shadow text-blue-500 shadow-gray-500 ' /></Link>
+           <Link to={"/"}><MdHome size={35}  className='m-2 border rounded-full shadow text-blue-500 shadow-gray-500 cursor-pointer ' /></Link>
            <Link to={"/user-orders"}><HiOutlineShoppingBag size={25} className='m-4'/></Link>
            <div>
           <div
@@ -46,4 +46,3 @@ const NavButton = () => {
 }
 
 export default NavButton
-
