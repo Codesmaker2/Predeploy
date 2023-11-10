2456b5ea8817db82188b32f66f73496f13dff715
Codesmaker2 <144585960+Codesmaker2@users.noreply.github.com>
2023-11-11 00:54:53 +0800

    Update NavButton.jsx

--- frontend/src/components/Route/Hero/NavButton.jsx
+++ frontend/src/components/Route/Hero/NavButton.jsx
@@ -1,46 +1,60 @@
 import React, { useState } from 'react'
-import { BiMessageDots, BiUser } from 'react-icons/bi'
-import { CgShoppingCart } from 'react-icons/cg'
-import { HiOutlineShoppingBag } from 'react-icons/hi'
-import { MdHome } from 'react-icons/md'
-import { Link } from 'react-router-dom'
+import { BiMessageDots, BiShoppingBag } from 'react-icons/bi'
+import { CgProfile, CgShoppingCart } from 'react-icons/cg'
 import Cart from '../../cart/Cart'
 import { useSelector } from 'react-redux'
+import { TbSmartHome } from 'react-icons/tb'
 
 const NavButton = () => {
     const [openCart, setOpenCart] = useState(false);
     const { cart } = useSelector((state) => state.cart);
   return (
-    <div className="fixed h-[10vh] bottom-0 left-0 w-full bg-white border-t border-gray-300 rounded-t-[20px] 800px:hidden ">
-    <div className="container mx-auto  ">
-      <div className="flex w-full">
-        <div className="flex w-full ">
-          <div className="flex grid-flow-col-5 justify-between w-full">
-           <Link to={"/profile"}><BiUser size={30} className='m-4 rounded-t-lg rounded-b-none'/></Link>
-           <Link to={"/inbox"}><BiMessageDots size={30} className='m-4'/></Link>
-           <Link to={"/"}><MdHome size={45}  className='m-2 border rounded-full shadow text-blue-500 shadow-gray-500 cursor-pointer outline-none tap:bg-white tap:text-white ' /></Link>
-           <Link to={"/user-orders"}><HiOutlineShoppingBag size={30} className='m-4'/></Link>
+    <div className="fixed h-[10vh] bottom-0 px-9 py-1  w-full bg-white border-t-[2px] border-blue-600 rounded-t-[20px] 800px:hidden ">
+    <div className=" ">
+      <div className="flex items-center justify-between w-full list-none">
+      <li><CgProfile size={25}/></li>
+      <li><BiMessageDots size={25}/></li>
+      <li><TbSmartHome size={40} color='blue' className='border rounded-full p-0.5'/></li>
+      <li><BiShoppingBag size={25}/></li>
+      <li><CgShoppingCart size={25} onClick={() => setOpenCart(true)}/></li>
+      <span className="absolute right-7 top-3 rounded-full bg-[#ec3a3a] w-4 h-4  text-white font-mono text-[12px] leading-tight text-center">
+              {cart && cart.length}
+            </span>
+        
+         {/* cart popup */}
+         {openCart ? <Cart setOpenCart={setOpenCart} /> : null}
+      </div>
+      
+      <div className='flex items-center justify-between list-none w-full  text-[0.8em] '>
+      <li>Me</li>
+      <li>Inbox</li>
+      <li className='ml-1.5'>_____</li>
+      <li>Order</li>
+      <li>Cart</li>
+    </div>
+    </div>
+   
+    {/* <div className="flex items-center justify-between w-full">
+           <Link to={"/profile"}><BiUser size={25} className='m-4 rounded-t-lg rounded-b-none'/></Link>
+           <Link to={"/inbox"}><BiMessageDots size={25} className='m-4'/></Link>
+           <Link to={"/"}><MdHome size={40}  className='m-2 border rounded-full shadow text-blue-500 shadow-gray-500 cursor-pointer tap:bg-white outline-none ' /></Link>
+           <Link to={"/user-orders"}><HiOutlineShoppingBag size={25} className='m-4'/></Link>
            <div>
           <div
-            className="relative mr-[20px]"
+            className="relative mr-[25px]"
             onClick={() => setOpenCart(true)}
           >
-            <CgShoppingCart  className=" m-1.5 mt-4"
-            size={30} />
-            <span className="absolute right-0 top-0 rounded-full bg-[#ec3a3a] w-4 h-4  text-white font-mono text-[12px]  leading-tight text-center">
+            <CgShoppingCart  className=" mb-3 mt-4"
+            size={25} />
+            <span className="absolute right-0 top-0 rounded-full bg-[#ec3a3a] w-4 h-4  text-white font-mono text-[12px] leading-tight text-center">
               {cart && cart.length}
             </span>
           </div>
         </div>
-        {/* cart popup */}
-        {openCart ? <Cart setOpenCart={setOpenCart} /> : null}
+       
 
-          </div>
-        
-        </div>
-        
-      </div>
-    </div>
+          </div> */}
+
   </div>
   )
 }
