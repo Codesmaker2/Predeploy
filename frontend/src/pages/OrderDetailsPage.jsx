24c623305c4f7859decab47fa1a4fd9cf9e0ae86
Codesmaker2 <144585960+Codesmaker2@users.noreply.github.com>
2023-11-18 22:50:52 +0800

    Update OrderDetailsPage.jsx

--- frontend/src/pages/OrderDetailsPage.jsx
+++ frontend/src/pages/OrderDetailsPage.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
-import Header from '../components/Layout/Header'
-import Footer from '../components/Layout/Footer'
+
 import UserOrderDetails from "../components/UserOrderDetails";
+import HeaderOrderdetails from './HeaderOrderdetails';
+import CustomerDetails from './CustomerDetails';
 
 const OrderDetailsPage = () => {
   return (
-    <div className="!overflow-scroll h-[100vh] bg-white">
-     
-      <br/>
+    <div className='bg-white'>
+     <HeaderOrderdetails/>
         <UserOrderDetails />
-        <Footer />
+        <CustomerDetails/>
     </div>
+    
   )
 }
 
