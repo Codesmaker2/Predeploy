3866aa2980f46d05f2bb61b9992713b221e6633d
Codesmaker2 <144585960+Codesmaker2@users.noreply.github.com>
2023-11-18 23:17:54 +0800

    Update CheckoutPage.jsx

--- frontend/src/pages/CheckoutPage.jsx
+++ frontend/src/pages/CheckoutPage.jsx
@@ -8,7 +8,7 @@ import { RxPinLeft } from 'react-icons/rx';
 
 const CheckoutPage = () => {
   return (
-    <div>
+   <div className='!overflow-scroll h-[90vh]'>
       <div className='w-full bg-blue-500 h-10 max-400px:hidden'></div>
       <Link to={"/dashboard-walkin-order-product"}>
         <RxPinLeft size={30} className='m-5 hover:text-blue-500 max-400px:hidden'/>
