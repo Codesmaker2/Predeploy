56d92da9a14315ff826bbd1ead58746bf3c65585
Codesmaker2 <144585960+Codesmaker2@users.noreply.github.com>
2023-11-19 22:04:56 +0800

    Update Checkout.jsx

--- frontend/src/components/Checkout/Checkout.jsx
+++ frontend/src/components/Checkout/Checkout.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import styles from "../../styles/styles";
 // import { Country, State } from "country-state-city";
-import { useNavigate } from "react-router-dom";
+import { Link,useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { useEffect } from "react";
 import axios from "axios";
