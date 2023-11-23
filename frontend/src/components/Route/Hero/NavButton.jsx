fa29ebef86665cf240a9e005b1027d45fb67b668
Codesmaker2 <144585960+Codesmaker2@users.noreply.github.com>
2023-11-23 21:40:23 +0800

    Update NavButton.jsx

--- frontend/src/components/Route/Hero/NavButton.jsx
+++ frontend/src/components/Route/Hero/NavButton.jsx
@@ -17,10 +17,11 @@ const NavButton = () => {
       <Link to={"/inbox"}><li><BiMessageDots size={25} className=' cursor-pointer'/></li></Link>
       <Link to={"/"}><li><TbSmartHome size={40} color='blue' className='border rounded-full p-0.5 cursor-pointer'/></li></Link>
       <Link to={"/user-orders"}><li><BiShoppingBag size={25} className=' cursor-pointer'/></li></Link>
-      <li><CgShoppingCart size={25} className=' cursor-pointer' onClick={() => setOpenCart(true)}/></li>
+      <li><CgShoppingCart size={25} className=' cursor-pointer' onClick={() => setOpenCart(true)}/>
       <span className="absolute right-7 top-3 rounded-full bg-[#ec3a3a] w-4 h-4  text-white font-mono text-[12px] leading-tight text-center">
               {cart && cart.length}
             </span>
+      </li>
         
          {/* cart popup */}
          {openCart ? <Cart setOpenCart={setOpenCart} /> : null}
