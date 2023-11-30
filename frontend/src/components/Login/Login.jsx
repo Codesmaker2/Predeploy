805e8f44fed5aaab626221c3681dc032ee46a2ca
Codesmaker2 <144585960+Codesmaker2@users.noreply.github.com>
2023-11-30 20:13:49 +0800

    Update Login.jsx

--- frontend/src/components/Login/Login.jsx
+++ frontend/src/components/Login/Login.jsx
@@ -103,7 +103,7 @@ const Login = () => {
                   required
                   value={email}
                   onChange={(e) => setEmail(e.target.value)}
-                  className=" h-9 appearance-none block w-full px-3 py-2 border border-blue-500 rounded-full shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+                  className=" h-10 appearance-none block w-full px-3 py-2 border border-blue-500 rounded-full shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
                 />
               </div>
             </div>
@@ -123,7 +123,7 @@ const Login = () => {
                   required
                   value={password}
                   onChange={(e) => setPassword(e.target.value)}
-                  className=" h-9 appearance-none block w-full px-3 py-2 border border-blue-500 rounded-full shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+                  className=" h-10 appearance-none block w-full px-3 py-2 border border-blue-500 rounded-full shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
                 />
                 {visible ? (
                   <AiOutlineEye
